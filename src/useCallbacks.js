de29771e573868c85133d7444d81219e5c83e51a
panda <panda.chen.2021@gmail.com>
2022-12-19 17:14:04 +0000

    update output list on callBack component

--- src/useCallbacks.js
+++ src/useCallbacks.js
@@ -6,20 +6,22 @@ function OutputList({ getContents }) {
     useEffect(() => {
         setListitem(getContents());
         console.dir(listItem);
+        console.dir(getContents());
     }, [getContents]);
  
-    return listItem.map((item, index) => <div key={index}> {item} </div>)
+    return listItem.map((item, index) => isNaN(item) ? <div key={index}>""</div> : <div key={index}> {item} </div>)
 }
 
 export default function CallbackComponent () {
-    const [num, setNum] = useState("");
+    //if put useState() empty, system will pop up a warning that "a component is changing an uncontrolled input to be controlled."
+    //best way to solve this issue: def a type of data on this useState, e.g. useState(0)
+    const [num, setNum] = useState();
     const [bgLight, setBgLight] = useState(true);
-
 
     //!!!import the different between useMemo and useCallback
     // useMemo => return the valu of that function;
     // useCallback => it takes a function but that is actually and return this function
-    const getNum = useCallback((num) => {
+    const getNum = useCallback(() => {
         return [Number(num)-1, Number(num), Number(num)+1];
     }, [num]);
 
@@ -31,6 +33,13 @@ export default function CallbackComponent () {
     return(
         <>
             <div value = "useCallbackFunc" style={bgStyle}>
+                <h2>useCallback</h2>
+                <p>The different between useCallback and useMemo</p>
+                <ul>
+                    <li>useMemo -> return the value of that function </li>
+                    <li>useCallback -> it takes a function but that is actually and return this function</li>
+                </ul>
+                <pre>e.g.</pre>
                 <input type={"number"} value={num} placeholder={"please input a number"} onChange={e => setNum(e.target.value)}></input>
                 <button onClick={() => setBgLight(preBg => !preBg)}>Toggle background to {bgLight ? "dark" : "light"} theme</button>
                 <OutputList getContents={getNum}/>
